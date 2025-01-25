9062f732deb4c820ede230b9bd963c44c5d6d5d4
Patricia <patriciastarck@gmail.com>
2025-01-25 16:14:39 -0300

    alterando limite para 100 e diminuí fonte do H1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-numeroLimite = 10;
+numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 // estou mexendo no html diretamente por aqui
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do numero secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
 }
 exibirMensagemInicial();
 
